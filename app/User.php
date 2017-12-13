10e3bae5885c1610147328c50b30526bbedf732e
Maximiliano Zapata <maxzap@users.noreply.github.com>
2017-12-13 16:47:00 -0300

    borrar

--- app/User.php
+++ app/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 <<<<<<< HEAD
     public function likes()
@@ -41,6 +42,8 @@ class User extends Authenticatable
     }
 
 >>>>>>> parent of 262a836... Merge branch 'master' into vistas
+=======
+>>>>>>> parent of 191de29... Likes OK
 =======
 >>>>>>> parent of 191de29... Likes OK
 }
